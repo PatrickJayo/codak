d71494ff721827c6f4692b71d18b996758310406
Patrick Jayoma <PatrickJayy@gmail.com>
2021-07-21 14:00:48 -0700

    Deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://codak-backend.herokuapp.com/posts' });
+const API = axios.create({ baseURL: 'https://codak-backend.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
